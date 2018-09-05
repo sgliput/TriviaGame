3a514e31ecf146e0415415030ea9fc0784b6549d
sgliput <sgliput@comcast.net>
2018-09-05 16:33:56 -0400

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ var rights = 0;
 var wrongs = 0;
 var questionsAnswered = 0;
 var levelCount = 0;
+var unansweredQ = 0;
 
 function introGenerator(objArray) {
     $("#Intro").show();
@@ -23,6 +24,15 @@ function getAnswerScreen(objArray) {
 
         $("#fruitimgAnswer").attr("src", objArray[questionsAnswered].image);
         $(".next").text(objArray[questionsAnswered].nextq);
+
+        if (objArray[questionsAnswered].isChecked && $(':checked').val() === objArray[questionsAnswered].answer) {
+            $(".result").text("Congratulations! That's correct!");
+            rights++;
+        } else {
+            $(".result").html("Sorry! That's incorrect!");
+            wrongs++;
+        }
+
     } else if (levelCount === 1) {
         $("#l2").hide();
         $("#answerScreen2").show();
@@ -30,6 +40,29 @@ function getAnswerScreen(objArray) {
         $("#varietyListAnswer").text(objArray[questionsAnswered].varieties);
         $(".next2").text(objArray[questionsAnswered].nextq);
 
+
+            $(".img1").attr("src", objArray[questionsAnswered].img1);
+            $(".img2").attr("src", objArray[questionsAnswered].img2);
+            $("#captionimg1").text(objArray[questionsAnswered].caption1);
+            $("#captionimg2").text(objArray[questionsAnswered].caption2);
+            $(".figure1").show();
+            $(".figure2").show();
+            if (objArray[questionsAnswered].answer == "Tomato") {
+                $(".option3Answer").append("&nbsp;<strong><em>Yes, it's a fruit!</em></strong>");
+            }
+        
+            if(stopwatch2.time === 0){
+                $(".result").html("Sorry! You ran out of time! The answer is actually <strong>" + objArray[questionsAnswered].answer + "</strong>.");
+                questionsAnswered++;
+                unansweredQ++;
+            } else if (objArray[questionsAnswered].isChecked && $(':checked').val() === objArray[questionsAnswered].answer) {
+                $(".result").text("Congratulations! That's correct!");
+                rights++;
+            } else {
+                $(".result").html("Sorry! That's incorrect! The answer is actually <strong>" + objArray[questionsAnswered].answer + "</strong>.");
+                wrongs++;
+            }
+        
     }
 
 
@@ -44,29 +77,9 @@ function getAnswerScreen(objArray) {
     $(".option3Answer").text(objArray[questionsAnswered].options[2]);
     $(".option4Answer").text(objArray[questionsAnswered].options[3]);
 
+    
 
-
-    if (objArray[questionsAnswered].isChecked && $(':checked').val() === objArray[questionsAnswered].answer) {
-        $(".result").text("Congratulations! That's correct!");
-        rights++;
-        if (levelCount === 1) {
-            $(".img1").attr("src", objArray[questionsAnswered].img1);
-            $(".img2").attr("src", objArray[questionsAnswered].img2);
-            $("#captionimg1").text(objArray[questionsAnswered].caption1);
-            $("#captionimg2").text(objArray[questionsAnswered].caption2);
-            $(".figure1").show();
-            $(".figure2").show();
-            if (objArray[questionsAnswered].answer == "Tomato") {
-                $(".option3Answer").append("&nbsp;<strong><em>Yes, it's a fruit!</em></strong>");
-            }
-        }
-
-    } else {
-        $(".result").text("Sorry! That's incorrect!");
-        wrongs++;
-        $(".figure1").hide();
-        $(".figure2").hide();
-    }
+    
 
 
 
@@ -131,19 +144,35 @@ function check(objArray) {
 };
 
 function getResultsScreen(objArray) {
+    $("#l1").hide();
+    $("#l2").hide();
     $("#answerScreen").hide();
     $("#answerScreen2").hide();
     $("#resultsScreen").show();
     $("#howManyRight").text(rights);
     $("#howManyWrong").text(wrongs);
+    stopwatch.stop();
+    answered = rights + wrongs;
+    if (stopwatch.time === 0 && answered === 11) {
+        $("#howManyWrongP").after("<p id='howManyUnanswered'>But you didn't answer 1 question.</p>");
+    } else if (stopwatch.time === 0) {
+        $("#howManyWrongP").after("<p id='howManyUnanswered'>But you didn't answer " + (12 - answered) + " questions.</p>");
+    };
+    if (unansweredQ === 1) {
+        $("#howManyWrongP").after("<p id='howManyUnanswered'>But you didn't answer 1 question.</p>");
+    } else if (unansweredQ > 1) {
+        $("#howManyWrongP").after("<p id='howManyUnanswered'>But you didn't answer " + unansweredQ + " questions.</p>");
+    };
+
     if (rights > 9) {
-        $("#verdict").text("Congratulations! You've unlocked Level 2!");
+        $("#verdict").text("Congratulations! You've unlocked " + intros[levelCount].nextLevel + "!");
         $("#whatNext").text("Orange you glad you know your fruit?");
         $(".nextLevel").text("Next Level");
     } else {
         $("#verdict").text("Sorry, your fruit knowledge needs some work.");
         $("#whatNext").text("Click to try again.");
         $(".nextLevel").text("Start Over");
+
     };
 };
 
@@ -187,21 +216,24 @@ var intros = [
         heading: "Welcome to Level 1",
         category: "Name That Fruit!",
         firstp: "So you think you know fruit?",
-        secondp: "Sure, you know an apple from a banana, but what about more exotic varieties? See if you can identify these less familiar options."
+        secondp: "Sure, you know an apple from a banana, but what about more exotic varieties? See if you can identify these less familiar options. You only have two minutes to finish this level, so good luck!",
+        nextLevel: "Level 2"
     },
 
     level2 = {
         heading: "Welcome to Level 2",
         category: "All Aboard the Type Train!",
         firstp: "How many kinds can one fruit have?",
-        secondp: "Quite a few, actually, and you might have eaten some without even knowing their name. Try to pick the fruit that comes in the following varieties:"
+        secondp: "Quite a few, actually, and you might have eaten some without even knowing their name. In this level, you only have ten seconds for each question before the answer is revealed. Try to pick the fruit that comes in the following varieties before time runs out!",
+        nextLevel: "Level 3"
     },
 
     level3 = {
         heading: "Welcome to Level 3",
         category: "Real Trivia Time!",
         firstp: "Now that we've covered exotic fruits and various cultivars, let's see how much you know about other fruit facts.",
-        secondp: "Whether it be fruity roles in pop culture or weird and wonderful truths you never knew you wanted to know, this is the level for true fruit trivia buffs."
+        secondp: "Whether it be fruity roles in pop culture or weird and wonderful truths you never knew you wanted to know, this is the level for true fruit trivia buffs.",
+        nextLevel: "Level 4"
     }
 
 ]
@@ -540,126 +572,229 @@ $(document).ready(function () {
         if (levelCount === 0) {
             updatePage(l1);
             stopwatch.start();
+
+
         } else if (levelCount === 1) {
             updatePage(l2);
+            stopwatch2.start();
+
         }
     });
-    $(".radio").on("click", function () {
+    $("#l1 .radio label").on("change", function () {
         check(l1);
-        check(l2);
-
-    });
-    $(".answer").on("click", function () {
         replace(l1);
         getAnswerScreen(l1);
         questionsAnswered++;
+
     });
     $(".next").on("click", function () {
         console.log(questionsAnswered);
         updatePage(l1);
     });
+
+
+
     $(".nextLevel").on("click", function () {
-        if (rights > 9) {
-            $("#resultsScreen").hide();
-            questionsAnswered = 0;
+        $("#resultsScreen").hide();
+        questionsAnswered = 0;
+            
+
+        if (levelCount === 0 && rights > 9) {
+            $("#displayTimer").text("00:10");
+            stopwatch2.time = 10;
             rights = 0;
             wrongs = 0;
             levelCount++;
             introGenerator(intros);
-        } else {
-            $("#resultsScreen").hide();
-            introGenerator(intros);
-            questionsAnswered = 0;
+            
+        } else if(levelCount === 1 && rights > 9) {
+            $("#displayTimer").hide();
             rights = 0;
             wrongs = 0;
+            levelCount++;
+            introGenerator(intros);
+
+        } else if (rights < 9) {
+            $("#howManyUnanswered").remove();
+            unansweredQ = 0;
+            rights = 0;
+            wrongs = 0;
+            introGenerator(intros);
+
+            if(levelCount === 0){
+            stopwatch.reset();
+            } else if (levelCount === 1){
+                stopwatch2.reset();
+            }
         }
     });
-    $(".answer2").on("click", function () {
+    $("#l2 .radio label").on("change", function () {
+        check(l2);
         replace(l2);
         getAnswerScreen(l2);
         questionsAnswered++;
-        //console.log(questionsAnswered);
+        stopwatch2.stop();
     });
     $(".next2").on("click", function () {
         console.log(questionsAnswered);
+        stopwatch2.reset();
         updatePage(l2);
+        stopwatch2.start();
+        if(questionsAnswered === 12){
+        stopwatch2.reset();
+        }
     });
 
 
 
 });
 
-window.onload = function() {
+    // Stopwatch objects
+    //  Variable that will hold setInterval that runs the stopwatch
+    var intervalId;
+    var clockRunning = false;
 
-    $("#stop").click(stopwatch.stop);
-    $("#reset").click(stopwatch.reset);
-    $("#starter").click(stopwatch.start);
-  };
+    //Stopwatch object
+    var stopwatch = {
 
-  //  Variable that will hold setInterval that runs the stopwatch
-  var intervalId;
-  var clockRunning = false;
-  
-  //Stopwatch object
-  var stopwatch = {
-  
-    time: 120,
-    lap: 1,
-  
-    reset: function() {
-  
-      stopwatch.time = 120;
-  
-      //  TODO: Change the "display" div to "02:00."
-      $("#displayTimer").text("02:00");
-      stopwatch.stop();
-    },
-  
-    start: function() {
-  
-      //  TODO: Use setInterval to start the count here and set the clock to running.
-      if (!clockRunning) {
-        intervalId = setInterval(stopwatch.count, 1000);
-        clockRunning = true;
-      }
-  
-    },
+        time: 120,
 
-    stop: function() {
-  
-      //clearInterval stops the count here and sets the clock to not be running.
-      clearInterval(intervalId);
-      clockRunning = false;
-    },
-  
-    count: function() {
-  
-      //Decrease time by one
-      stopwatch.time--;
-      //Converts time to timer format
-      var converted = stopwatch.timeConverter(stopwatch.time);
-      //Shows the converted time in the "display" div.
-      $("#displayTimer").text(converted);
-    },
-  
-    timeConverter: function(t) {
-  
-      //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-      var minutes = Math.floor(t / 60);
-      var seconds = t - (minutes * 60);
-  
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-  
-      if (minutes === 0) {
-        minutes = "00";
-      }
-  
-      else if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-  
-      return minutes + ":" + seconds;
-    }
-  };
+        reset: function () {
+
+            stopwatch.time = 120;
+
+            //  TODO: Change the "display" div to "02:00."
+            $("#displayTimer").text("02:00");
+            stopwatch.stop();
+        },
+
+        start: function () {
+
+            //  TODO: Use setInterval to start the count here and set the clock to running.
+            if (!clockRunning) {
+                intervalId = setInterval(stopwatch.count, 1000);
+                clockRunning = true;
+            }
+
+        },
+
+
+        stop: function () {
+
+            //clearInterval stops the count here and sets the clock to not be running.
+            clearInterval(intervalId);
+            clockRunning = false;
+        },
+
+        count: function () {
+
+            //Decrease time by one
+            stopwatch.time--;
+            //Converts time to timer format
+            var converted = stopwatch.timeConverter(stopwatch.time);
+            //Shows the converted time in the "display" div.
+            $("#displayTimer").text(converted);
+
+            if (stopwatch.time === 0) {
+                stopwatch.stop();
+                questionsAnswered = 12;
+                updatePage(l1);
+
+
+            }
+        },
+
+        timeConverter: function (t) {
+
+            //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+            var minutes = Math.floor(t / 60);
+            var seconds = t - (minutes * 60);
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+
+            if (minutes === 0) {
+                minutes = "00";
+            }
+
+            else if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+
+            return minutes + ":" + seconds;
+        }
+    };
+
+    var stopwatch2 = {
+
+        time: 10,
+
+        reset: function () {
+
+            stopwatch2.time = 10;
+
+            //  TODO: Change the "display" div to "02:00."
+            $("#displayTimer").text("00:10");
+            stopwatch2.stop();
+        },
+
+        start: function () {
+
+            //  TODO: Use setInterval to start the count here and set the clock to running.
+            if (!clockRunning) {
+                intervalId = setInterval(stopwatch2.count, 1000);
+                clockRunning = true;
+            }
+
+        },
+
+
+        stop: function () {
+
+            //clearInterval stops the count here and sets the clock to not be running.
+            clearInterval(intervalId);
+            clockRunning = false;
+        },
+
+        count: function () {
+
+            //Decrease time by one
+            stopwatch2.time--;
+            //Converts time to timer format
+            var converted = stopwatch2.timeConverter(stopwatch2.time);
+            //Shows the converted time in the "display" div.
+            $("#displayTimer").text(converted);
+
+            if (stopwatch2.time === 0) {
+                stopwatch2.stop();
+                getAnswerScreen(l2);
+
+
+            }
+        },
+
+        timeConverter: function (t) {
+
+            //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+            var minutes = Math.floor(t / 60);
+            var seconds = t - (minutes * 60);
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+
+            if (minutes === 0) {
+                minutes = "00";
+            }
+
+            else if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+
+            return minutes + ":" + seconds;
+        }
+    };
+
+
+
